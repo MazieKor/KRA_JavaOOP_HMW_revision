3f7c889f0a05757011a800180c686b9bcfb90eb5
Maciej Korczala <maciej.coders@gmail.com>
2021-08-25 22:21:42 +0200

    adding getters and setters- task 3 (.5-6)

--- src/main/java/pl/coderslab/homeworks/oop/first/Book.java
+++ src/main/java/pl/coderslab/homeworks/oop/first/Book.java
@@ -23,5 +23,35 @@ public class Book {
         this.additionalAuthors = additionalAuthors;
     }
 
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public boolean getAvailable(){
+        return this.available;
+    }
+    public void setAvailable(boolean available){
+        this.available = available;
+    }
+    public Author getAuthor(){
+        return author;
+    }
+    public void setAuthor(Author author){
+        this.author = author;
+    }
+    public Author[] getAdditionalAuthors(){
+        return additionalAuthors;
+    }
+    public void setAdditionalAuthors(Author[] additionalAuthors){
+        this.additionalAuthors = additionalAuthors;
+    }
 
 }
